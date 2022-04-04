20d0080ed4615f06aad174e718b677fc7d928be0
sosalover <91343823+sosalover@users.noreply.github.com>
2022-04-03 19:01:36 -0500

    adding style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -27,7 +28,7 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
@@ -38,7 +39,7 @@ const Banner = ({ title }) => (
 );
 
 const CourseList = ({ courses }) => (
-  <div>
+  <div className="course-list">
     { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
@@ -54,8 +55,11 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
