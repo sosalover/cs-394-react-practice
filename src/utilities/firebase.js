a58ecfcae4fb1c4db3e21ba70a73d6d4a37cc2ae
sosalover <91343823+sosalover@users.noreply.github.com>
2022-04-07 11:20:17 -0500

    read from a database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_ObY6jscYJF8VhWFtLmGJjiKhspZIg8s",
+    authDomain: "cs-394-react-practice.firebaseapp.com",
+    databaseURL: "https://cs-394-react-practice-default-rtdb.firebaseio.com",
+    projectId: "cs-394-react-practice",
+    storageBucket: "cs-394-react-practice.appspot.com",
+    messagingSenderId: "247249623795",
+    appId: "1:247249623795:web:3133801b54aa6cc1e5f586",
+    measurementId: "G-GXNCL4KTBC"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
